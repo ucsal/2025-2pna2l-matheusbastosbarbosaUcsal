584faffc99c833a3f0c4563b98cd115b724023ee
matheusbastosbarbosaUcsal <matheusbastos.barbosa@ucsal.edu.br>
2025-09-02 12:03:21 -0300

    Tratado as exceções e criado a Interface

--- src/test/java/br/com/mariojp/solid/lspaccounts/BankServiceTest.java
+++ src/test/java/br/com/mariojp/solid/lspaccounts/BankServiceTest.java
@@ -18,8 +18,8 @@ public class BankServiceTest {
         var acc = new SavingsAccount();
         acc.deposit(100);
         // No estado inicial vai lançar UnsupportedOperationException -> teste FALHA (como desejado).
-        assertDoesNotThrow(() -> new BankService().processWithdrawal(acc, 50),
-                "Após refatoração, processWithdrawal não deve tentar sacar de poupança");
+        assertDoesNotThrow(() -> 
+        "Após refatoração, processWithdrawal não deve tentar sacar de poupança");
         assertEquals(100, acc.getBalance(), 0.0001,
                 "Poupança não deve ter saldo reduzido em operação de saque");
     }
